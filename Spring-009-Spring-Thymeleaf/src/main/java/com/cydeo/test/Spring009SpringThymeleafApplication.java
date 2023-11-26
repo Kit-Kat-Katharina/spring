93a827ff418b2ae3249f634afc8e5153402131d7
HP Laptop <katharina.slava@gmail.com>
2023-11-25 19:59:34 -0500

    commit-spring-009

---
+++ Spring-009-Spring-Thymeleaf/src/main/java/com/cydeo/test/Spring009SpringThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring009SpringThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring009SpringThymeleafApplication.class, args);
+    }
+
+}
---
+++ Spring-009-Spring-Thymeleaf/src/test/java/com/cydeo/test/Spring009SpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring009SpringThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
