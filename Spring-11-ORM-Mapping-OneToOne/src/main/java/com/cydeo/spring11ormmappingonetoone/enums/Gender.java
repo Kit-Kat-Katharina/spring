4e569935ae6b3ff9eb791c835ae38df500c0367a
HP Laptop <katharina.slava@gmail.com>
2023-12-22 13:14:38 -0500

    committing-queries

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/spring11ormmappingonetoone/enums/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.cydeo.spring11ormmappingonetoone.enums;
-
-public enum Gender {
-    M, F;
-}
--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/spring11ormmappingonetoone/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cydeo.spring11ormmappingonetoone.repository;
-
-import com.cydeo.spring11ormmappingonetoone.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
